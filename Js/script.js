0769982ac802e52b79ecff4d6203d173600f4d5c
Reidner Willians Pacheco Peres <pacheco195920@gmail.com>
2023-02-23 19:42:30 -0300

    swiper

--- Js/script.js
+++ Js/script.js
@@ -5,4 +5,22 @@ let navbar = document.querySelector('.header .navbar');
 
 window.onscroll = () =>{
     navbar.classList.remove('active');  
-}    
+} 
+
+var swiper = new Swiper(".home-slider", {
+    loop:true,
+    effect: "coverflow",
+    grabCursor: true,
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
